3ef78a636ad4f1b3350894dce32aecd992e14f16
EC2 Default User <ec2-user@ip-172-31-3-120.ap-northeast-1.compute.internal>
2022-06-04 08:06:43 +0000

    fourth commit

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,12 +4,14 @@ class ListsController < ApplicationController
   end
 
   def create
-    list = List.new(list_params)
-    list.save
-
-    redirect_to list_path(list.id)
-
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
     end
+
+  end
 
     def index
       @lists = List.all
@@ -29,10 +31,16 @@ class ListsController < ApplicationController
       redirect_to list_path(list.id)
     end
 
+    def destroy
+      list = List.find(params[:id])  # データ（レコード）を1件取得
+      list.destroy  # データ（レコード）を削除
+      redirect_to '/lists'  # 投稿一覧画面へリダイレクト
+    end
+
     private
 
     def list_params
-      params.require(:list).permit(:title, :body)
+      params.require(:list).permit(:title, :body, :image)
     end
 
 end
