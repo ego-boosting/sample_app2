7b09469544a24fef69df30e2c214b911131984d6
EC2 Default User <ec2-user@ip-172-31-3-120.ap-northeast-1.compute.internal>
2022-06-04 03:22:56 +0000

    third commit

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
   get 'lists/:id' => 'lists#show',as:'list'
-  get 'lists/:id/edit' => 'list#edit',as:'edit_list'
+  get 'lists/:id/edit' => 'lists#edit',as:'edit_list'
+  patch 'lists/:id' => 'lists#update',as:'update_list'
 
 end
